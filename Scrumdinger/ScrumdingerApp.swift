22992104677a88d386f50816e0d2369cd4a1c192
Blair Duddy <blairduddy@hotmail.co.uk>
2023-04-12 20:46:42 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Blair Duddy on 2023-04-12.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
